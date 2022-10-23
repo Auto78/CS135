637407cf45d03c2269fd6c44b8ea8152a3e5afbd
Auto78 <94161885+Auto78@users.noreply.github.com>
2022-10-22 17:27:42 -0700

    Update Assignment5.cpp

--- Assignment5.cpp
+++ Assignment5.cpp
@@ -1,3 +1,19 @@
+/*
+	Name: Alvaro Cruz, 5005488379, 1005, ASSIGNMENT 5
+	Description:
+	Input: User input:
+	Output: 
+*/
+//Preprocessor Directives
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <limits>
+
+using namespace std;
+
+//Intro to Main Code:
+int main(){
+    
+    //Variable Declaration
+}
