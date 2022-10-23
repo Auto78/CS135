1cac420eaacf86c81477e019c859fdc051e18139
Auto78 <94161885+Auto78@users.noreply.github.com>
2022-10-22 18:24:26 -0700

    Update Assignment5.cpp

--- Assignment5.cpp
+++ Assignment5.cpp
@@ -1,10 +1,10 @@
 /*
-	Name: Alvaro Cruz, 5005488379, 1005, ASSIGNMENT 5
-	Description:
-	Input: User input:
-	Output: 
+    Name: Alvaro Cruz, 5005488379, 1005, ASSIGNMENT 5
+    Description:
+    Input: User input:
+    Output:
 */
-//Preprocessor Directives
+// Preprocessor Directives
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -14,24 +14,41 @@ using namespace std;
 
 // I think I could use arrays but its not needed so attempt without and see how it goes 10/22
 
-//Intro to Main Code:
-int main(){
-    
-    //Variable Declaration
-    ifstream inFile; //Input file stream variable
-    ofstream outFile; //Output file stream variable
+// Intro to Main Code:
+int main()
+{
+
+    // Variable Declaration
+    ifstream inFile;  // Input file stream variable
+    ofstream outFile; // Output file stream variable
+    string fileName;  // Name of file to be opened
     string licensePlate = " ";
     string date = " ";
     string time = " ";
     int weight = 0;
     int speed = 0;
 
-    while (!inFile.is_open())
+//Loop asks user to open a file and checks that it opens
+    do
     {
-        
+        cout << "Open File: ";
+        getline(cin, fileName);
+        inFile.open(fileName);
+        if (!inFile.is_open())
+        {
+            cout << "Could not open nonexistant\n";
+            inFile.clear();
+            inFile.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+        break;
+    } while (!inFile.is_open());
+    //Simple testing to see that the file opened
+    if (inFile.is_open())
+    {
+        cout << "you opened a file " << fileName;
+        cout << endl;
     }
     
-
-
-
+    
 }
