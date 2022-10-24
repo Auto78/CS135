7f40e8d5f6bef22ed77cffa6335485fcc08f710c
Auto78 <94161885+Auto78@users.noreply.github.com>
2022-10-23 17:38:33 -0700

    Update Assignment5.cpp

--- Assignment5.cpp
+++ Assignment5.cpp
@@ -9,10 +9,12 @@
 #include <iomanip>
 #include <fstream>
 #include <limits>
+#include <climits>
 
 using namespace std;
 
-// I think I could use arrays but its not needed so attempt without and see how it goes 10/22
+// Use arrays
+const int ARRAY_SIZE = INT_MAX;
 
 // Intro to Main Code:
 int main()
@@ -22,13 +24,14 @@ int main()
     ifstream inFile;  // Input file stream variable
     ofstream outFile; // Output file stream variable
     string fileName;  // Name of file to be opened
-    string licensePlate = " ";
-    string date = " ";
-    string time = " ";
-    int weight = 0;
-    int speed = 0;
+    string licensePlate[ARRAY_SIZE] = {};
+    string date[ARRAY_SIZE] = {};
+    string time[ARRAY_SIZE] = {};
+    int weight[ARRAY_SIZE] = {};
+    int speed[ARRAY_SIZE] = {};
+    int dataTracker = 0;
 
-//Loop asks user to open a file and checks that it opens
+    // Loop asks user to open a file and checks that it opens
     do
     {
         cout << "Open File: ";
@@ -43,12 +46,50 @@ int main()
         }
         break;
     } while (!inFile.is_open());
-    //Simple testing to see that the file opened
-    if (inFile.is_open())
+    // While loop takes in input until EoF
+    while (!inFile.eof())
     {
-        cout << "you opened a file " << fileName;
-        cout << endl;
+        getline(inFile, licensePlate[dataTracker], ',');
+        getline(inFile, date[dataTracker], ',');
+        getline(inFile, time[dataTracker], ',');
+        inFile >> weight[dataTracker];
+        inFile >> speed[dataTracker];
+        // Error Checker accounting for endl at eof
+        if (!inFile)
+        {
+            if (!inFile.eof())
+            {
+                inFile.clear();
+                inFile.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+            continue;
+        }
+        dataTracker++;
     }
-    
-    
+    // Testing
+    for (int i = 0; i < dataTracker; i++)
+    {
+        cout << "Licenses: " << licensePlate[i];
+    }
+    cout << endl;
+    for (int i = 0; i < dataTracker; i++)
+    {
+        cout << "Dates: " << date[i];
+    }
+    cout << endl;
+    for (int i = 0; i < dataTracker; i++)
+    {
+        cout << "Times: " << time[i];
+    }
+    cout << endl;
+    for (int i = 0; i < dataTracker; i++)
+    {
+        cout << "Weights: " << weight[i];
+    }
+    cout << endl;
+    for (int i = 0; i < dataTracker; i++)
+    {
+        cout << "Speed: " << speed[i];
+    }
+    cout << endl;
 }
