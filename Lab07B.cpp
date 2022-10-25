721b9f33dd5b3ddc638e61f390ce24ff1352e555
Auto78 <94161885+Auto78@users.noreply.github.com>
2022-10-25 15:38:16 -0700

    Create Lab07B.cpp

---
+++ Lab07B.cpp
@@ -0,0 +1,23 @@
+/*
+    Name: Alvaro Cruz, 5005488379, 1005, Lab 7B
+    Description:
+    Input:
+    Output:
+*/
+
+//Preprocessor Directives
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include<limits>
+
+using namespace std;
+
+//Declaring Const Variables
+
+//Intro to main code
+int main(){
+
+    //varibale Declaration
+    
+}
