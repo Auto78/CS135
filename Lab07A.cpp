72ec915169f17c88f9997f82d046f019d1dd6f21
Auto78 <94161885+Auto78@users.noreply.github.com>
2022-10-25 13:49:44 -0700

    Update Lab07A.cpp

--- Lab07A.cpp
+++ Lab07A.cpp
@@ -9,32 +9,65 @@
 #include <iostream>
 #include <fstream>
 #include <limits>
+//Switch to doubles Fixed with SetP
+#include<iomanip>
 
 using namespace std;
 // Declaring const variables
-
+const int ROW = 7;
+const int COLUMN = 10;
 // Intro to main code
 int main()
 {
-
     // Variable declaration
-    ifstream inFile;
+    ifstream iFile;
     string fileName = " ";
+    int arr[ROW][COLUMN];
 
     // loop for checking file is open
     do
     {
         cout << "Enter a file name\n**";
         getline(cin, fileName);
-        inFile.open(fileName);
+        iFile.open(fileName);
 
-        if (!inFile.is_open())
+        if (!iFile.is_open())
         {
             cout << "\nError invalid file name\n";
-            inFile.clear();
-            inFile.ignore(numeric_limits<streamsize>::max(), '\n');
+            iFile.clear();
+            iFile.ignore(numeric_limits<streamsize>::max(), '\n');
             continue;
         }
         break;
-    } while (!inFile.is_open());
+    } while (!iFile.is_open());
+    // Storing Data in the 2D Array
+    // Note: having r be row and c be column helps me keep track better
+    for (int r = 0; r < ROW; r++)
+    {
+        for (int c = 0; c < COLUMN; c++)
+        {
+            iFile >> arr[r][c];
+        }
+    }
+    // Printing Data from the 2D Array
+    // Row to colum based
+    for (int r = 0; r < ROW; r++)
+    {
+        for (int c = 0; c < COLUMN; c++)
+        {
+            cout << arr[r][c] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    // Colum to Row based
+    for (int c = 0; c < COLUMN; c++)
+    {
+        for (int r = 0; r < ROW; r++)
+        {
+            cout << arr[r][c] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
