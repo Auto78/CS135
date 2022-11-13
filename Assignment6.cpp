9b790645168579c8601c9a77b3c547c9d690a0cd
Auto78 <94161885+Auto78@users.noreply.github.com>
2022-11-12 19:03:43 -0800

    Update Assignment6.cpp

--- Assignment6.cpp
+++ Assignment6.cpp
@@ -1,10 +1,10 @@
 /*
-          Name: Alvaro A. Cruz, #5005488379, 1005, Labo08B
-          Description: Program displays information about a circle
-          given a radius within certain range. Implementing various
-          self-declared functions.
-          Input:User inputs radius of circle
-          Output: Displays Circumference, Area, and radius
+          Name: Alvaro A. Cruz, #5005488379, 1005, Assignment 6
+          Description: Program plays a game of Crossword(with replayability) given a file containing 
+          the crossword. Performs task using a variety of self-made functions and vectors. 
+          Input:User inputs "level" to play and guess to be placed on crossword
+          Output: Displays current crossword game board and number of incorrect
+          guess left.
 */
 
 // Preprocessor Directives
@@ -13,7 +13,6 @@
 #include <vector>
 #include <limits>
 #include <fstream>
-#include <algorithm>
 
 using namespace std;
 // Function Protoypes
@@ -29,36 +28,36 @@ bool exhuastedGuesses(vector<char>, char);
 int main()
 {
     // Variable Declaration
-    ifstream inFile;  // Input file stream variable
-    char guess = ' '; // Letter to Check
-
-    int incorrectGuess = 5; // Number of incorrect guess avaliable
+    ifstream inFile;
+    char guess = ' ';
+    int incorrectGuess = 5;
     char playAgain = ' ';
     bool playAgainFlag = false;
     bool gameOver = false;
-    vector<vector<char> > answerKey;
-    vector<vector<char> > gameBoard; // Vector conatians the level in a gameboard style
+    vector<vector<char> > answerKey; // Vector will contain solution to crossword
+    vector<vector<char> > gameBoard; // Vector contains current gameboard to crossword
     vector<char> exhaustedGuess;    // Vector contains the used guesses
 
-    //  Display Game Logo
-    displayLogo();
-    // Tracks what level is currently playing;
-    openLevel(inFile);
-    gameBoard = readLevel(inFile);
-    answerKey = gameBoard;               // This vector contains the answerkey before replacing the gameboard to only # & _
-    gameBoard = replaceBoard(gameBoard); // Replacing the gameboard with inital Status; # & _
-    printsBoard(gameBoard);
-    // Asking user for letter to check
-    // Fix this it Works but need to do error checking
+    // OutputPhase
+
+    displayLogo();                       // Display Game Logo
+    openLevel(inFile);                   // Open the level to be played;
+    gameBoard = readLevel(inFile);       // Input the crossword answer key into the gameboard
+    answerKey = gameBoard;               // Store the answerkey before replacing the gameboard to only # & _
+    gameBoard = replaceBoard(gameBoard); // Replacing the gameboard to inital Status; # & _
+    printsBoard(gameBoard);              // Prints the starting board
+
+    // Outer Do while Performs the main game
     do
     {
 
-        // Checks for ending game
+        // Checks for Game Over condition
         if (incorrectGuess == 0)
         {
             cout << "play again? (y/n) ";
             cin >> playAgain;
             playAgain = toupper(playAgain);
+            // Clear everything if Replaying
             if (playAgain == 'Y')
             {
                 cout << endl;
@@ -70,17 +69,18 @@ int main()
                 cin.clear();
                 openLevel(inFile);
                 gameBoard = readLevel(inFile);
-                answerKey = gameBoard;               // This vector contains the answerkey before replacing the gameboard to only # & _
-                gameBoard = replaceBoard(gameBoard); // Replacing the gameboard with inital Status; # & _
+                answerKey = gameBoard;
+                gameBoard = replaceBoard(gameBoard);
                 printsBoard(gameBoard);
                 continue;
             }
+            // Print message and end program if Not replaying
             if (playAgain == 'N')
             {
                 cout << "\nThank you for playing CrossWord!\n";
                 exit(0);
-                break;
             }
+            // Error Message for invalid entry
             if (playAgain != 'Y' || 'N')
             {
                 cout << "\nInvalid entry!\n";
@@ -89,11 +89,13 @@ int main()
                 continue;
             }
         }
+        // Checks for Victory Condition
         if (gameBoard == answerKey)
         {
             cout << "Play again? (y/n) ";
             cin >> playAgain;
             playAgain = toupper(playAgain);
+            // Clear everything if Replaying
             if (playAgain == 'Y')
             {
                 cout << endl;
@@ -105,17 +107,18 @@ int main()
                 cin.clear();
                 openLevel(inFile);
                 gameBoard = readLevel(inFile);
-                answerKey = gameBoard;               // This vector contains the answerkey before replacing the gameboard to only # & _
-                gameBoard = replaceBoard(gameBoard); // Replacing the gameboard with inital Status; # & _
+                answerKey = gameBoard;
+                gameBoard = replaceBoard(gameBoard);
                 printsBoard(gameBoard);
                 continue;
             }
+            // Print message and end program if not Replaying
             if (playAgain == 'N')
             {
                 cout << "\nThank you for playing CrossWord!\n";
                 exit(0);
-                break;
             }
+            // Print error message for invalid entry
             if (playAgain != 'Y' || 'N')
             {
                 cout << "\nInvalid entry!\n";
@@ -124,22 +127,22 @@ int main()
                 continue;
             }
         }
-        // Main part of the game
+        // If statment Performs game if there is not game over?
         if (gameOver == false)
         {
+            // Do-While loop performs the game
             do
             {
-
                 cout << "\nEnter a letter:\n";
                 cin >> guess;
                 cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 guess = toupper(guess);
-                // Checks if letter is an alphebitc and then adds to the loop
+                // Checks if letter is an alphabetic
                 if (isalpha(guess))
                 {
                     bool usedGuess = exhuastedGuesses(exhaustedGuess, guess);
                     bool onBoard = onTheBoard(answerKey, guess);
-
+                    // Places guess on board if it is on the board
                     if (onBoard == true)
                     {
                         for (size_t i = 0; i < answerKey.size(); i++)
@@ -152,51 +155,56 @@ int main()
                                 }
                             }
                         }
-                        // Printing stuff if the letter is not on the board
+                        // Prints message if the guess was already used
                         if (usedGuess == true)
                         {
                             cout << "The letter is already guessed, try again!\n";
                         }
                     }
-                    // Prints info if its not on the board
+                    // Prints message if the guess wasn't on the board
                     if (onBoard == false)
                     {
-                        // Printing stuff if the letter is not on the board
+                        // If the guess was used, prints message
                         if (usedGuess == true)
                         {
                             cout << "The letter is already guessed, try again!\n";
                         }
+                        // If not on the board & guess !used yet print message and decrease guess
                         else
                         {
                             cout << "The letter is not on the board\n";
                             incorrectGuess--;
                         }
                     }
-                    // Checks if its been gess then print
                 }
-
-                cout << "Remaining incorrect guesses: " << incorrectGuess << endl;
-                cout << endl;
+                // Prints current number of guess left
+                cout << "Remaining incorrect guesses: "
+                     << incorrectGuess << "\n\n";
+                // Stores used guess in vector
                 exhaustedGuess.push_back(guess);
+                // Prints current game board
                 printsBoard(gameBoard);
+                // Messages for win conditions
+                // Prints message if game won
                 if (gameBoard == answerKey)
                 {
                     cout << "\nCongratulations! you solved the level!\n";
                 }
+                // Prints message if lost game
                 if (incorrectGuess == 0)
                 {
                     cout << "\nBetter luck next time!\n";
                 }
-
             } while (false);
         }
-
     } while (!playAgainFlag);
 }
 
-// Prints gameboard
-
-// Function to Print game Logo
+/*
+         FUNCTION_IDENTIFIER: Void function displays game logo
+         parameters: None, Void function
+         return value: Non-Returning; Prints to screen
+*/
 void displayLogo()
 {
     cout << endl
@@ -206,7 +214,12 @@ void displayLogo()
          << setw(11) << "R\n"
          << setw(12) << "D\n\n";
 }
-// Opens file according to Level and returns the level currently on.
+/*
+         FUNCTION_IDENTIFIER: Opens "level"(file) to play given user input
+         given they are within the correct parameters.
+         parameters: Pass by reference, ifstream variable(inFile)
+         return value: Non-returning; Opens file being passed
+*/
 void openLevel(ifstream &inFile)
 {
     int level = 0;
@@ -221,20 +234,25 @@ void openLevel(ifstream &inFile)
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             continue;
         }
-        if (level <= 3 && level > 0)
+        if (level > 0)
         {
             inFile.open("level" + to_string(level) + ".txt");
-        }
-        if (level >= 4)
-        {
-            cout << "Level files could not be found!\n";
+            if (!inFile.is_open())
+            {
+                cout << "Level files could not be found!\n";
+            }
             continue;
         }
 
     } while (!inFile.is_open());
     cout << endl;
 }
-// Hopefully this places the vector values in the right place
+/*
+         FUNCTION_IDENTIFIER: Function reads the file contents and
+         stores them into a 2D Vector, once done closes file.
+         parameters: Pass by reference; ifstream(inFile)
+         return value: Returns 2D vector with stored information
+*/
 vector<vector<char> > readLevel(ifstream &inFile)
 {
     vector<vector<char> > solutions2D;
@@ -251,7 +269,12 @@ vector<vector<char> > readLevel(ifstream &inFile)
     inFile.close();
     return solutions2D;
 }
-// Makes replacements to the board having it start as # and _ only
+/*
+         FUNCTION_IDENTIFIER: Function replaces board with _'s
+         parameters: Pass 2D vector
+         return value: Returns 2D vector with certain contents
+         replaced with _'s
+*/
 vector<vector<char> > replaceBoard(vector<vector<char> > board)
 {
 
@@ -267,7 +290,11 @@ vector<vector<char> > replaceBoard(vector<vector<char> > board)
     }
     return board;
 }
-// Prints the gameboard
+/*
+         FUNCTION_IDENTIFIER: Function prints given board to the screen
+         parameters: 2D vector
+         return value: Non-Returning; Prints current board to screen
+*/
 void printsBoard(vector<vector<char> > board)
 {
     for (size_t i = 0; i < board.size(); i++)
@@ -279,7 +306,12 @@ void printsBoard(vector<vector<char> > board)
         cout << endl;
     }
 }
-// Checks if letter is on the board;
+/*
+         FUNCTION_IDENTIFIER: Boolean Function checks that char is on the answer key
+         returns true if it is else false
+         parameters: 2D vector containing answerkey, char containing guess
+         return value: Returns true if the guess is found; else returns false
+*/
 bool onTheBoard(vector<vector<char> > answerKey, char guess)
 {
     for (size_t i = 0; i < answerKey.size(); i++)
@@ -294,7 +326,13 @@ bool onTheBoard(vector<vector<char> > answerKey, char guess)
     }
     return false;
 }
-
+/*
+         FUNCTION_IDENTIFIER: Function checks vector to see if
+         it contains the current guess being passed
+         parameters: 1D vector containing used guesses,
+         char containing the current guess to be checked
+         return value: Returns true if vector contains guess; Else returns false
+*/
 bool exhuastedGuesses(vector<char> exhaustedGuess, char guess)
 {
     for (size_t i = 0; i < exhaustedGuess.size(); i++)
