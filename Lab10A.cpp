ec2b5fd6a5bd960b0914a15f4900cb782f407694
Auto78 <94161885+Auto78@users.noreply.github.com>
2022-11-22 18:21:00 -0800

    Create Lab10A.cpp

---
+++ Lab10A.cpp
@@ -0,0 +1,62 @@
+/*
+Name: Alvaro Cruz, #5005488379, 1008, Lab2A
+Description:
+Input:
+Output:
+*/
+
+// Preprocessor Directives
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+// Structs
+struct studentType
+{
+    string firstName;
+    string lastName;
+    int grade;
+    char letterGrade;
+};
+
+// Function Protoypes
+void getStudentData(studentType &);
+void getLetterGrade(studentType &);
+void printStudentData(studentType);
+
+// Intro to main code;
+int main()
+{
+
+    // Instance of Struct
+    studentType kid;
+}
+
+// Intro to user Defined Functions
+
+/*
+          FUNCTION_IDENTIFIER: FUNCTION_DESCRIPTION.
+          parameters: WHAT_TO_PASS_FUNCTION
+          return value: WHAT_FUNCTION_RETURNS
+*/
+void getStudentData(studentType &kid)
+{
+}
+
+/*
+          FUNCTION_IDENTIFIER: FUNCTION_DESCRIPTION.
+          parameters: WHAT_TO_PASS_FUNCTION
+          return value: WHAT_FUNCTION_RETURNS
+*/
+void getLetterGrade(studentType &kid)
+{
+}
+/*
+          FUNCTION_IDENTIFIER: FUNCTION_DESCRIPTION.
+          parameters: WHAT_TO_PASS_FUNCTION
+          return value: WHAT_FUNCTION_RETURNS
+*/
+void printStudentData(studentType kid)
+{
+}
