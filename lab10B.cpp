03352d34a38b552bd69bc4c6a2ae3ea956409a9b
Auto78 <94161885+Auto78@users.noreply.github.com>
2022-11-29 12:59:53 -0800

    Update lab10B.cpp

--- lab10B.cpp
+++ lab10B.cpp
@@ -1,8 +1,9 @@
 /*
 Name: Alvaro Cruz, #5005488379, 1008, Lab10B
-Description:
-Input:
-Output:
+Description: Function uses substring, find, files, and structs to gather information,
+store into array of struct type, and print information to screen all via functions.
+Input: User inputs file to be open
+Output: Prints information on screen neatly on a table.
 */
 
 // Preprocessor Directives
@@ -12,11 +13,6 @@ Output:
 #include <limits>
 
 using namespace std;
-
-// Function Prototypes
-void openFile(ifstream &, string);
-void readFile(ifstream&, PurchaseType[]);
-void printPurchaseData(PurchaseType[], int);
 
 // Structs
 struct PurchaseType
@@ -28,21 +24,32 @@ struct PurchaseType
     double taxRate;
 };
 
+// Function Prototypes
+void openFile(ifstream &, string);
+void readFile(ifstream &, PurchaseType[]);
+void printPurchaseData(PurchaseType[], int);
+void printTable();
+
+// constants
+const int ARRAY_SIZE = 11;
+
 // Intro to main code
 int main()
 {
+    int fileSize = 10;
     ifstream inFile;
-    string prompt = "Enter a file name\n**";
+    string prompt = "Enter file name\n**";
+    PurchaseType purchases[ARRAY_SIZE];
     openFile(inFile, prompt);
+    readFile(inFile, purchases);
+    printPurchaseData(purchases, fileSize);
 }
 
 /*
-          FUNCTION_IDENTIFIER: Function prompts user for file name
-          and then opens the file.
-          parameters: ifstream and string for the prompt
-          return value: Void: Non-Returning
+        FUNCTION_IDENTIFIER: Ask user for file to open
+        parameters: ifstream by reference and string for the prompt
+        return value: Non-Returning
 */
-
 void openFile(ifstream &inFile, string prompt)
 {
     string fileName;
@@ -58,4 +65,94 @@ void openFile(ifstream &inFile, string prompt)
         }
         break;
     } while (true);
+}
+/*
+    FUNCTION_IDENTIFIER: Reads the file 
+    parameters: ifstream by reference and arrray of stuct type
+    return value: Non-Returning
+*/
+void readFile(ifstream &inFile, PurchaseType purchases[])
+{
+    int numberOfRows = 0;
+
+    while (!inFile.eof())
+    {
+        string tempString = "";
+        while (getline(inFile, tempString))
+        {
+            int locationComma = tempString.find(',');
+
+            purchases[numberOfRows].customerID = tempString.substr(0, locationComma);
+
+            // Product Name
+            tempString = tempString.substr(locationComma + 1, tempString.length());
+            locationComma = tempString.find(',');
+            purchases[numberOfRows].productName = tempString.substr(0, locationComma);
+
+            // Prices
+            tempString = tempString.substr(locationComma + 1, tempString.length());
+            locationComma = tempString.find(',');
+            purchases[numberOfRows].price = stod(tempString.substr(0, locationComma));
+
+            // Quantity
+            tempString = tempString.substr(locationComma + 1, tempString.length());
+            locationComma = tempString.find(',');
+            purchases[numberOfRows].qtyPurchased = stoi(tempString.substr(0, locationComma));
+
+            // tax
+            tempString = tempString.substr(locationComma + 1, tempString.length());
+            locationComma = tempString.find(',');
+            purchases[numberOfRows].taxRate = stod(tempString.substr(0, locationComma));
+
+            numberOfRows++;
+        }
+    }
+    inFile.close();
+}
+
+/*
+        FUNCTION_IDENTIFIER: Prints all the data to screen
+        parameters:array of declared struct type and integer
+        return value: Non-Returning
+*/
+void printPurchaseData(PurchaseType purchases[], int fileSize)
+{
+    double total = 0.0;
+    double sumTotal = 0.0;
+    double averageTotal = 0.0;
+    int uniqueId = 0;
+    printTable();
+    for (int i = 0; i < fileSize; i++)
+    {
+        cout << "|" << setw(12) << purchases[i].customerID << " | ";
+        cout << setw(12) << purchases[i].productName << " |";
+        cout << fixed << setprecision(2) << setw(6) << purchases[i].price << " | ";
+        cout << setw(8) << purchases[i].qtyPurchased << " | ";
+        cout << setw(8) << purchases[i].taxRate * 100 << " |";
+        total = ((purchases[i].price * purchases[i].qtyPurchased) * (purchases[i].taxRate + 1));
+        cout << setw(7) << total << " |\n";
+        sumTotal += total;
+
+        if (purchases[i].customerID != purchases[i - 1].customerID)
+        {
+            uniqueId++;
+        }
+
+        averageTotal = sumTotal / fileSize;
+    }
+    cout << "+-------------+--------------+-------+----------+----------+--------+\n";
+    cout << "Unique users:  " << uniqueId << endl;
+    cout << "Average total: $" << averageTotal << endl;
+}
+/*
+        FUNCTION_IDENTIFIER: Prints the first half of the table
+        parameters: none
+        return value: Non-Returning 
+*/
+void printTable()
+{
+
+    cout << "\n+-------------+--------------+-------+----------+----------+--------+\n"
+         << "| Customer ID | Product Name | Price | Quantity | Tax Rate | Total  | \n"
+         << "+-------------+--------------+-------+----------+----------+--------+\n";
 }
